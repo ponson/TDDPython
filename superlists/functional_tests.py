884fdeeb314d3b0d42d9d37fe4b8c692071dc2b0
Ponson Sun <ponsonsun@gmail.com>
2023-08-27 17:40:45 +0800

    add *.log to ignore for not tracking. add unittest module.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Congratulations' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('TO-DO', self.browser.title)
+        self.fail('Finish the test!')
+
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
