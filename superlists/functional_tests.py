0b283214cbdaf7e0099b45b378de88cccd4682d1
Ponson Sun <ponsonsun@gmail.com>
2023-09-04 23:05:17 +0800

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import unittest
 
 
@@ -18,19 +19,20 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
         # header_text = self.browser.find_element_by_tag_name('h1').text
-        header_text = self.browser.find_element('name','h1').text
-        self.assertIn('TO-DO', header_text)
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do', header_text)
 
         # inputbox = self.browser.find_element_by_id('id_newitem')
-        inputbox = self.browser.find_element('id', 'id_newitem')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         # table = self.browser.find_element_by_id('id_list_table')
         table = self.browser.find_element('id', 'id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        # rows = table.find_elements_by_tag_name('tr')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
         self.fail('Finish the test!')
 
         
