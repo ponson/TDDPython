3b159b15819717793eb4eab4b28f5b177fab597f
Ponson Sun <ponsonsun@gmail.com>
2023-10-01 21:18:54 +0800

    Done.

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -5,7 +6,7 @@ import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -22,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
         
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title, "Browser Title is:%s" % (self.browser.title))
         # header_text = self.browser.find_element_by_tag_name('h1').text
         header_text = self.browser.find_element(By.TAG_NAME,'h1').text
@@ -41,11 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         time.sleep(5)
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
-
-        
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
