915b9240a960756963e5fbb414267cf161c67a57
Ponson Sun <ponsonsun@gmail.com>
2023-10-05 23:22:55 +0800

    The functional test has done.

--- superlists/functional_tests/test.py
+++ superlists/functional_tests/test.py
@@ -35,6 +35,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
 
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
         self.browser.refresh()
 
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
@@ -45,4 +48,24 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+        #new user Francis coming
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #Francis enter home page, there is no Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        
+        #Francis add a new item
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
         self.fail('Finish the test!')
